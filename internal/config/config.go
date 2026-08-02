b1a7d1c6db3aa122b867852517c25a33854f6e3a
x x <i80980@qq.com>
2026-08-02 21:31:27 +0800

    chore(release): 更新版本到 0.6.0

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,7 +16,7 @@ import (
 const (
 	ProtocolVersion    = "2026-07-28"
 	ServerName         = "agentdock"
-	Version            = "0.5.4"
+	Version            = "0.6.0"
 	PathModel          = "host"
 	BrowserRunnerDir   = "browser-runner"
 	BrowserArtifactDir = "browser-artifacts"
